540eda875dd27b0aeb0a9943567b0f0aa5505e0b
sjha1101 <codingchannel8i@gmail.com>
2025-07-06 11:57:11 +0530

    Initial commit

---
+++ demo.js
@@ -0,0 +1,62 @@
+// a=9;
+// console.log(a);
+
+// namw="surbhi jha";
+// //dynamically typed
+
+// //let,var,const
+
+// {
+//     let a=9;
+//     a=90;
+//     // const b=10;
+//     //b=2; error
+//     console.log(a);
+   // console.log(b);//will genrate an error
+// }
+
+// const b=99;
+// console.log(b);
+
+// function show(){
+//     var a=34;
+// }
+
+//let const ---blocked scoped variables
+//var ---function scoped variables
+
+// "use strict"
+// a=9;
+// console.log(a);
+
+// let a=90;
+// const b =10;
+
+// {
+//     let a=9;
+//     console.log(a);
+
+// }
+
+
+// console.log(a);
+
+// if(a==9)
+// {
+//     console.log("It's true");
+// }else{
+//     console.log("It's false");
+// }
+
+// for(const i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+var a=1;
+
+{
+    var a=2;
+}
+console.log(a);
+
+//variable hoisting
