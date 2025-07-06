197c095ed2e2363cefaf5917ce4d114a9ce1d481
sjha1101 <codingchannel8i@gmail.com>
2025-07-06 13:23:32 +0530

    Add JS Practice Set 1  solutions

---
+++ 01_Introduction_to_js.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+console.log("This function is used to add 3 number")
+function addThreeNumbers(a,b,c) {
+    return a+b+c
+}
+
+let c = addThreeNumbers(1,2,3)
+console.log(c)
