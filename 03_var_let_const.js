bf71b233058327bc30d5f2facbc86102ccdb608b
sjha1101 <codingchannel8i@gmail.com>
2025-07-06 18:19:37 +0530

    variables in js

---
+++ 03_var_let_const.js
@@ -0,0 +1,30 @@
+/*we can declare variables using:
+var, let, and const 
+var
+The value of var can be updated*/
+var a = 45
+var a = "hello";
+console.log(a)
+
+//let
+//The value of let cannot be updated
+let b = 45
+//let b = "hello"; //Not allowed
+b = 100 //Allowed
+console.log(b)
+
+//const
+//const abc = 45
+abc = "hello" //Not allowed
+//const abc; //Not allowed
+
+let c = null
+console.log(c)
+let d = undefined
+
+{
+    let b = "this"
+    console.log(b)
+}
+
+console.log(b)
