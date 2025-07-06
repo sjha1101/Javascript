67b59c718ae2c1d2aead4d57342f809b143112ae
sjha1101 <codingchannel8i@gmail.com>
2025-07-06 16:21:04 +0530

    Add JS Practice Set 2 solutions

---
+++ 06_operator.js
@@ -0,0 +1,61 @@
+//Operator in js
+console.log("Arithmetic Operator")
+let a = 10
+let b = 5
+console.log("a + b = ",a+b)
+console.log("a - b = ",a-b)     
+console.log("a * b = ",a*b)
+console.log("a / b = ",a/b)
+console.log("a % b = ",a%b)
+console.log("a ** b = ",a**b)
+
+//After a++ here addition will be done afterward
+console.log("a++ = ",a++) //op: 10
+console.log("a-- = ",a--) //op: 9
+
+//After ++a here addition will be done before the variable
+console.log("++a = ",++a) //op: 11
+console.log("--a = ",--a) //op: 10
+
+console.log("Assignment Operator")
+let c = 5 
+let d = 5
+
+console.log("c = d = ",c=d)
+console.log("c += d = ",c+=d)
+console.log("c -= d = ",c-=d)
+console.log("c *= d = ",c*=d)
+console.log("c /= d = ",c/=d)
+console.log("c %= d = ",c%=d)
+console.log("c **= d = ",c**=d)
+
+let e = "2"
+let f = 2
+
+console.log("Comparison Operator")
+//check the value 
+console.log("e == f = ",e==f)
+console.log("e != f = ",e!=f)
+//check the value and datatype
+console.log("e === f = ",e===f)
+console.log("e !== f = ",e!==f)
+console.log("e > f = ",e > f)
+console.log("e < f = ",e < f)
+console.log("e >= f = ",e >= f)
+console.log("e <= f = ",e <= f)
+console.log("e ?? f = ",e ?? f)
+
+console.log("Logical Operator")
+let g = true
+let h = false
+
+console.log("g && h = ",g && h)
+console.log("g || h = ",g || h)
+console.log("!g = ",!g)
+
+//signle line comment
+// comments in the js 
+
+//Multiline comment
+/* comments in the 
+js */
