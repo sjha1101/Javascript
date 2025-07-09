447b3b828a798c05f8db041c66c56b8ee663641c
sjha1101 <codingchannel8i@gmail.com>
2025-07-09 13:04:22 +0530

    String in js

---
+++ 13_Strings.js
@@ -0,0 +1,19 @@
+//String 
+let name = "Rohan"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let friendname = 'Rohit'
+console.log(friendname)
+/* let name2 = "Ravi' this will generate error
+console.log(name2)*/
+
+//Template literals is used to print string with variables is also called string interpolation
+let name3 = "Ravi"
+let age = 19
+console.log(`My name is ${name3} and my age is ${age}`) 
+
+//Escape character is used to print string 
+let allEscapes = "Hello World!\nNew Line\tTab Space\nPath: C:\\folder\\file\nSingle Quote: \' \nDouble Quote: \" ";
+console.log(allEscapes);
+
