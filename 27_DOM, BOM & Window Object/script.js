c7fcf5abacddf1e6b5b62ed32a33ecac39a52501
sjha1101 <codingchannel8i@gmail.com>
2025-07-10 22:50:18 +0530

    DOM, BOM & Window Object in js

---
+++ 27_DOM, BOM & Window Object/script.js
@@ -0,0 +1,8 @@
+//window is used to access the window object & window is global object 
+console.log(window)
+window.console.log("Hello")
+
+//Document is used to access the document object it access full html page
+console.log(document)
+console.log(document.body)
+document.body.style.backgroundColor = "yellow"
